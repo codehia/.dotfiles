30f1ed8a6a07e8478806b1ed8698cb93fb91411f
Soumyaranjan Acharya <dev@sacharya.dev>
2023-10-08 15:52:01 +0530

    add bluetooth Mpris, picom

--- .local/bin/update-xorg-conf.sh
+++ .local/bin/update-xorg-conf.sh
@@ -1,22 +1,8 @@
 #!/bin/bash
-touch /etc/X11/xorg.conf.d/00-keyboard.conf
 touch /etc/X11/xorg.conf.d/20-amdgpu.conf
 touch /etc/X11/xorg.conf.d/30-touchpad.conf
 
-cat<<EOF>>/etc/X11/xorg.conf.d/00-keyboard.conf
-# Written by systemd-localed(8), read by systemd-localed and Xorg. It's
-# probably wise not to edit this file manually. Use localectl(1) to
-# instruct systemd-localed to update it.
-Section "InputClass"
-        Identifier "system-keyboard"
-        MatchIsKeyboard "on"
-        Option "XkbLayout" "us"
-        Option "XkbModel" "pc105+inet"
-        Option "XkbOptions" "caps:escape,terminate:ctrl_alt_bksp"
-EndSection
-EOF
-
-cat<<EOF>>/etc/X11/xorg.conf.d/20-amdgpu.conf
+cat <<EOF >>/etc/X11/xorg.conf.d/20-amdgpu.conf
 Section "Device"
      Identifier "AMD"
      Driver "amdgpu"
@@ -24,7 +10,7 @@ Section "Device"
 EndSection
 EOF
 
-cat<<EOF>>/etc/X11/xorg.conf.d/30-touchpad.conf
+cat <<EOF >>/etc/X11/xorg.conf.d/30-touchpad.conf
 Section "InputClass"
     Identifier "touchpad"
     Driver "libinput"
