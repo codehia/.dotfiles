ff83396f1dafc11ea1ed9bc92f49b5c66db801bb
codehia <dev@sacharya.dev>
2025-03-24 14:25:12 +0530

    Add dadbod

--- dot_config/nvim/lua/plugins/telescope.lua
+++ dot_config/nvim/lua/plugins/telescope.lua
@@ -10,11 +10,6 @@ return {
   },
   config = function()
     require('telescope').setup {
-      -- pickers = {
-      --   find_files = {
-      --     theme = 'ivy',
-      --   },
-      -- },
       extensions = {
         fzf = {},
         ['ui-select'] = {
@@ -31,7 +26,9 @@ return {
     local builtin = require 'telescope.builtin'
     vim.keymap.set('n', '<leader>sh', builtin.help_tags, { desc = '[S]earch [H]elp' })
     vim.keymap.set('n', '<leader>sk', builtin.keymaps, { desc = '[S]earch [K]eymaps' })
-    vim.keymap.set('n', '<leader>sf', builtin.find_files, { desc = '[S]earch [F]iles' })
+    vim.keymap.set('n', '<leader>sf', function()
+      builtin.find_files { hidden = true }
+    end, { desc = '[S]earch [F]iles' })
     vim.keymap.set('n', '<leader>ss', builtin.builtin, { desc = '[S]earch [S]elect Telescope' })
     vim.keymap.set('n', '<leader>sw', builtin.grep_string, { desc = '[S]earch current [W]ord' })
     vim.keymap.set('n', '<leader>sg', builtin.live_grep, { desc = '[S]earch by [G]rep' })
@@ -40,16 +37,15 @@ return {
     vim.keymap.set('n', '<leader>s.', builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
     vim.keymap.set('n', '<leader>sm', builtin.buffers, { desc = '[S]earch [M]arks' })
     vim.keymap.set('n', '<leader><leader>', builtin.buffers, { desc = '[ ] Find existing buffers' })
-    vim.keymap.set('n', '<leader>/', builtin.current_buffer_fuzzy_find, { desc = '[/] Fuzzily search in current buffer' })
 
     -- Slightly advanced example of overriding default behavior and theme
-    -- vim.keymap.set('n', '<leader>/', function()
-    --   -- You can pass additional configuration to Telescope to change the theme, layout, etc.
-    --   builtin.current_buffer_fuzzy_find(require('telescope.themes').get_dropdown {
-    --     winblend = 8,
-    --     previewer = false,
-    --   })
-    -- end, { desc = '[/] Fuzzily search in current buffer' })
+    vim.keymap.set('n', '<leader>/', function()
+      -- You can pass additional configuration to Telescope to change the theme, layout, etc.
+      builtin.current_buffer_fuzzy_find(require('telescope.themes').get_dropdown {
+        winblend = 10,
+        previewer = false,
+      })
+    end, { desc = '[/] Fuzzily search in current buffer' })
 
     -- It's also possible to pass additional configuration options.
     --  See `:help telescope.builtin.live_grep()` for information about particular keys
