a0c1ff863381a3e55101fd46c2e96c4cdb904941
codehia <dev@sacharya.dev>
2025-03-25 00:42:13 +0530

    Update bat, nvim and redshift, rofi

---
+++ dot_config/nvim/lua/lazy_config.lua
@@ -0,0 +1,21 @@
+return {
+  install = {
+    colorscheme = { 'catppuccin' },
+  },
+  ui = {
+    border = 'rounded',
+    title = 'Plugins',
+  },
+  performance = {
+    disabled_plugins = {
+      'gzip',
+      'matchit',
+      'matchparen',
+      'netrwPlugin',
+      'tarPlugin',
+      'tohtml',
+      'tutor',
+      'zipPlugin',
+    },
+  },
+}
